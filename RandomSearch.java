1f93ee43d6b584605b02a48e6eee9d444bf6b4fd
GloomyGeese <34144122+GloomyGeese@users.noreply.github.com>
2017-12-16 14:18:26 -0600

    Raw 1.1 Code

---
+++ RandomSearch.java
@@ -0,0 +1,62 @@
+package randosearch;
+
+import java.awt.Desktop;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+public class RandomSearch {
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+     Map<String,Integer> map = new HashMap<String,Integer>();
+     for(int i = 0; i < 1000; ++i)
+     {
+        String s = choose(new File("customization//searches.txt"));
+        if(!map.containsKey(s))
+           map.put(s, 0);
+        map.put(s, map.get(s) + 1);
+     }
+     Random generator = new Random();
+     Object[] values = map.values().toArray();
+     Object randomValue = values[generator.nextInt(values.length)]; // generates 1st random #
+     Object randomValue2 = values[generator.nextInt(values.length)]; // generates 2nd random #
+     Object name = getKeyFromValue(map, randomValue); // Associates 1st random # with a word
+     Object name2 = getKeyFromValue(map, randomValue2); // Associates 1st random # with a word
+     Desktop d = Desktop.getDesktop(); // short cut variable
+     
+     if (name == name2) // tests to not have duplicate words in search
+     {
+         Object randomValue3 = values[generator.nextInt(values.length)]; // generates 2nd random #
+         Object name3 = getKeyFromValue(map, randomValue3); // Associates 1st random # with a word
+         d.browse(new URI("https://www.google.com/search?q=" + name + "%20" + name3)); // Opens default browser with the google search query of the random values
+     }
+     else {
+     d.browse(new URI("https://www.google.com/search?q=" + name + "%20" + name2)); // Opens default browser with the google search query of the random values 
+     }
+  }
+
+  public static String choose(File f) throws FileNotFoundException // reads the list of words
+  {
+     String result = null;
+     Random rand = new Random();
+     int n = 0;
+     for(Scanner sc = new Scanner(f); sc.hasNext(); )
+     {
+        ++n;
+        String line = sc.nextLine();
+        if(rand.nextInt(n) == 0)
+           result = line;         
+     }
+
+     return result;      
+  }
+  public static Object getKeyFromValue(Map<String, Integer> hm, Object value) { // gets the word with the #
+	    for (Object o : hm.keySet()) {
+	      if (hm.get(o).equals(value)) {
+	        return o;
+	      }
+	    }
+	    return null;
+	  }
+	}
