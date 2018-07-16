6063e2825f94b4866553af0a0762079bf7bb7392
davidteather <34144122+davidteather@users.noreply.github.com>
2018-07-16 09:43:16 -0500

    Added Validation of User Inputs

--- pyrgs.py
+++ pyrgs.py
@@ -5,13 +5,24 @@ def random_line():
     lines = open('searches.txt').read().splitlines()
     myline = random.choice(lines)
     return myline
+def usr_input():
+    while True:
+        try:
+            tabs = int(input("How many random tabs would you like to open?" + os.linesep))
+        except ValueError:
+            print("That is not a valid integer." + os.linesep + "If you believe this is an error make an issue report on https://github.com/davidteather/Random-Google-Search/tree/RGS-Python" + os.linesep)
+            continue
+        else:
+            return tabs
+            break
 print ("By using this program you are taking full responsibilities for your own actions with this program and in no way is the creator or contributors liable for your own actions." + os.linesep)
-tabs = int(input("How many random tabs would you like to open?" + os.linesep))
-while (tabs > 0):
-    tabs -= 1
-    name = random_line()
-    name1 = random_line()
-    if name == name1:
+tabs1 = usr_input()
+if 1 == 1:
+    while (tabs1 > 0):
+        tabs1 -= 1
+        name = random_line()
         name1 = random_line()
-    url = 'https://www.google.com/search?q=' + name + "%20" + name1
-    webbrowser.open_new(url)
+        if name == name1:
+           name1 = random_line()
+        url = 'https://www.google.com/search?q=' + name + "%20" + name1
+        webbrowser.open_new(url)
